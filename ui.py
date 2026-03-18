7a9749e3dd0af1010614c19cb0553c3a2f72ef20
babaralishah <syedbabar957@gmail.com>
2026-03-18 18:12:15 +0000

    Update API_BASE_URL to use 0.0.0.0 for better accessibility

--- ui.py
+++ ui.py
@@ -1,8 +1,8 @@
 import os
 
 from src.config import TOP_K
-API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000")
-
+# API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000")
+API_BASE_URL = os.getenv("API_BASE_URL", "http://0.0.0.0:8000")
 
 import streamlit as st
 import requests
